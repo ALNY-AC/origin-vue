d56e57d5d88230f690465f385f00f6f7be5146df
代码狮 <1173197065@qq.com>
2018-12-26 01:10:38 +0800

    修复bug

--- bin/origin-vue.js
+++ bin/origin-vue.js
@@ -2,13 +2,16 @@
 var program = require('commander');
 var CreateComp = require('./CreateComp');
 var CreatePage = require('./CreatePage');
+var appInfo = require('./../package.json');
 
 program
-    .version('0.0.1')
+    .version(appInfo.version)
     .option('-c, --comp', '创建一个组件')
     .option('-n, --name <lang>', '定义组件或页面的名称')
     .option('-p, --page', '创建一个页面')
 program.parse(process.argv);
+
+console.log(process.versions);
 
 if (program.comp) {
     CreateComp();
